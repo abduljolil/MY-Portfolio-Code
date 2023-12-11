29d5c0979b1f32964cb8a3e5564919989e34047a
jalil56545 <jalil.mariya.31@gmail.com>
2023-12-11 14:30:40 +0600

    create a banner section

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -3,13 +3,17 @@
 const Banner = () => {
     return (
         <div className="hero min-w-screen relative" style={{ backgroundImage: 'url(../../public/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg)' }}>
-            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-overlay bg-opacity-80"></div>
+            <div className="hero-content flex-col lg:flex-row justify-center items-center">
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="mb-5 text-5xl font-bold">Hi ,My <br />name is Abdul Jolil</h1>
+                    <p className="mb-5">
+                        I'm MERN stact creative web developer from Pabna,Bangladesh
+                    </p>
                 </div>
+            </div>
+            <img className="w-90 h-60 rounded-full" src="../../public/young-man-classroom-character-scene_24877-57854.jpg" alt="" />
             </div>
         </div>
     );
